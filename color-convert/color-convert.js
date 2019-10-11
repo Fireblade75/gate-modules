48e63f729623bc7a75958ca7cdfd81f561b51021
Fireblade75 <Fireblade75@users.noreply.github.com>
2019-10-11 19:47:06 +0200

    color-convert

---
+++ color-convert/color-convert.js
@@ -0,0 +1,69 @@
+module.exports = function(RED) {
+    function ColorConvertNode(config) {
+        RED.nodes.createNode(this, config)
+        const node = this;
+        
+        this.on('input', function(msg) {
+            const color = (msg.payload.color || 'none').toLowerCase()
+            switch(color) {
+                case 'red': 
+                    node.send({payload: {
+                        red: 255,
+                        green: 0,
+                        blue: 0
+                    }})
+                    break;
+                case 'green': 
+                    node.send({payload: {
+                        red: 0,
+                        green: 255,
+                        blue: 0
+                    }})
+                    break;
+                case 'blue': 
+                    node.send({payload: {
+                        red: 0,
+                        green: 0,
+                        blue: 255
+                    }})
+                    break;
+                case 'yellow': 
+                    node.send({payload: {
+                        red: 255,
+                        green: 255,
+                        blue: 0
+                    }})
+                    break;
+                case 'cyan': 
+                    node.send({payload: {
+                        red: 0,
+                        green: 255,
+                        blue: 255
+                    }})
+                    break;
+                case 'purple': 
+                    node.send({payload: {
+                        red: 255,
+                        green: 0,
+                        blue: 255
+                    }})
+                    break;
+                case 'none': 
+                    node.send({payload: {
+                        red: 0,
+                        green: 0,
+                        blue: 0
+                    }})
+                    break;
+                default:
+                    node.error('Unknown color found: ' + color)
+                    node.send({payload: {
+                        red: 0,
+                        green: 0,
+                        blue: 0
+                    }})
+            }
+        })
+    }
+    RED.nodes.registerType("color-convert", ColorConvertNode)
+}
